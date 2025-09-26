73fd27ef42c8ebc6449fad10cb65e4eb63535f85
Tmk <tomasz.meczkowski@gmail.com>
2025-09-26 16:53:30 +0200

    init commit

---
+++ validator/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class PeselForm(forms.Form):
+    pesel = forms.CharField(
+        max_length=11,
+        min_length=11,
+        label="Numer PESEL",
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Wprowadź PESEL"}),
+        error_messages={
+            "required": "Podaj numer PESEL.",
+            "max_length": "PESEL musi mieć dokładnie 11 cyfr.",
+            "min_length": "PESEL musi mieć dokładnie 11 cyfr.",
+        }
+    )
