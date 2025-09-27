499de50cca4281e8db765f2e0ba3596514605669
Tmk <tomasz.meczkowski@gmail.com>
2025-09-27 17:39:59 +0200

    template buttons placement fix

--- validator/forms.py
+++ validator/forms.py
@@ -6,7 +6,12 @@ class PeselForm(forms.Form):
         max_length=11,
         min_length=11,
         label="Numer PESEL",
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Wprowadź PESEL"}),
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Wprowadź PESEL",
+            }
+        ),
         error_messages={
             "required": "Podaj numer PESEL.",
             "max_length": "PESEL musi mieć dokładnie 11 cyfr.",
